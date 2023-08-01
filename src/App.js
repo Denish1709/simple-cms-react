5ff10bc98f0c23309848b6952531774c8fe436e8
Denish1709 <denishrao22s@forward.edu.my>
2023-08-01 11:27:49 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/home";
+import Dashboard from "./pages/dashboard";
+import ManagePosts from "./pages/manage-posts";
+import ManagePostAdd from "./pages/manage-posts-add";
+import ManagePostEdit from "./pages/manage-posts-edit";
+import Post from "./pages/post";
+
+/*
+  / -> home
+  /dashboard -> Dashboard
+  /manage-posts -> ManagePosts
+*/
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/manage-posts" element={<ManagePosts />} />
+        <Route path="/manage-posts-add" element={<ManagePostAdd />} />
+        <Route path="/manage-posts-edit" element={<ManagePostEdit />} />
+        <Route path="/post" element={<Post />} />
+      </Routes>
+    </Router>
   );
 }
 
