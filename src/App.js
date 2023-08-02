cda81542624d2cd8b2a9ca8f526cc8463bed4355
Denish1709 <denishrao22s@forward.edu.my>
2023-08-02 12:02:41 +0800

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Dashboard from "./pages/dashboard";
 import ManagePosts from "./pages/manage-posts";
 import ManagePostAdd from "./pages/manage-posts-add";
 import ManagePostEdit from "./pages/manage-posts-edit";
+import ManageUsers from "./pages/manage-users";
+import ManageUsersAdd from "./pages/manage-users-add";
+import ManageUserEdit from "./pages/manage-users-edit";
 import Post from "./pages/post";
 
 /*
@@ -21,8 +24,12 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/manage-posts" element={<ManagePosts />} />
         <Route path="/manage-posts-add" element={<ManagePostAdd />} />
-        <Route path="/manage-posts-edit" element={<ManagePostEdit />} />
-        <Route path="/post" element={<Post />} />
+        <Route path="/manage-posts-edit/:id" element={<ManagePostEdit />} />
+        <Route path="./manage-posts" element={<ManagePosts />} />
+        <Route path="/post/:id" element={<Post />} />
+        <Route path="/manage-users" element={<ManageUsers />} />
+        <Route path="/manage-users-add" element={<ManageUsersAdd />} />
+        <Route path="/manage-users-edit/:id" element={<ManageUserEdit />} />
       </Routes>
     </Router>
   );
